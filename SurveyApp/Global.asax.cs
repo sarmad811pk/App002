0c24d29322d8e40f47f6fe4efa17ac67540ac355
shazebali <shazeb140@gmail.com>
2016-03-13 19:45:47 +0500

    test

--- SurveyApp/Global.asax.cs
+++ SurveyApp/Global.asax.cs
@@ -21,7 +21,7 @@ namespace SurveyApp
             AreaRegistration.RegisterAllAreas();
 
             //System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<SurveyApp.Models.SchoolContext>());
-
+            //test
             if (!WebSecurity.Initialized)
             {
                 WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
